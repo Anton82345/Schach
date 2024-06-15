3edd1ddddc6633be30c7291a743baff95d2b8c22
Anton <a.klonig@freenet.de>
2024-06-15 14:20:09 +0200

    Hintergrundmusik

--- src/home.java
+++ src/home.java
@@ -2,13 +2,22 @@ package src;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.sound.sampled.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.*;
-public class home extends JFrame implements ActionListener
+import java.io.File;
+import java.io.IOException;
+
+public class home extends JFrame implements ActionListener, ChangeListener
 {
     JLabel SpracheLabel, EndLabel, Hintergrund;
-    JButton Beginn, Wiederholen, HomeButton, SpracheAnnehmen;
+    JButton Beginn, Wiederholen, HomeButton, SpracheAnnehmen, MusikButton;
     JComboBox Sprache;
+    JSlider LautstarkeRegler;
     sprache sprache;
+    Clip musikClip;
+    FloatControl Lautstärke;
 
     public home()
     {
@@ -24,13 +33,6 @@ public class home extends JFrame implements ActionListener
         Hintergrund = new JLabel(new ImageIcon("lib/pic/Background1.jpeg"));
         Hintergrund.setBounds(0, 0, 800, 800);
         layeredPane.add(Hintergrund, Integer.valueOf(0));
-
-
-        Beginn = new JButton("spielen");
-        Beginn.setBounds(200, 200, 200, 50);
-        Beginn.addActionListener(this);
-        Beginn.setEnabled(false);
-        this.add(Beginn);
 
         Beginn = new JButton("spielen");
         Beginn.setBounds(200, 200, 200, 50);
@@ -53,7 +55,32 @@ public class home extends JFrame implements ActionListener
         SpracheAnnehmen.addActionListener(this);
         this.add(SpracheAnnehmen);
 
+        MusikButton = new JButton("Musik An/Aus");
+        MusikButton.setBounds(50, 700, 200, 50);
+        MusikButton.addActionListener(this);
+        this.add(MusikButton);
+
+        LautstarkeRegler = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        LautstarkeRegler.setBounds(50, 650, 200, 50);
+        LautstarkeRegler.addChangeListener(this);
+        this.add(LautstarkeRegler);
+
         this.setVisible(true);
+
+        try 
+        {
+            File musikDatei = new File("lib/sound/HintergrundMusik.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(musikDatei);
+            musikClip = AudioSystem.getClip();
+            musikClip.open(audioStream);
+            Lautstärke = (FloatControl) musikClip.getControl(FloatControl.Type.MASTER_GAIN);
+            setVolume(50);
+            musikClip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) 
+        {
+            e.printStackTrace();
+        }
     }
 
     public void actionPerformed(ActionEvent event)
@@ -74,6 +101,30 @@ public class home extends JFrame implements ActionListener
             Beginn.setText(sprache.Spielen());
             this.setTitle(sprache.Titel());
         }
+
+        if (event.getSource() == MusikButton) {
+            if (musikClip.isRunning()) {
+                musikClip.stop();
+            } else {
+                musikClip.start();
+            }
+        }
+    }
+
+    // Für Lautstärke
+    public void stateChanged(ChangeEvent e) {
+        if (e.getSource() == LautstarkeRegler) {
+            setVolume(LautstarkeRegler.getValue());
+        }
+    }
+
+    private void setVolume(int value) {
+        if (Lautstärke != null) {
+            float min = Lautstärke.getMinimum();
+            float max = Lautstärke.getMaximum();
+            float volume = min + (max - min) * (value / 100.0f);
+            Lautstärke.setValue(volume);
+        }
     }
 
     public static void main(String[] args) 
@@ -82,5 +133,3 @@ public class home extends JFrame implements ActionListener
         test.setVisible(true);
     }
 }
-
-
